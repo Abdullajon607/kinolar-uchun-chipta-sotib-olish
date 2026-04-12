b5632f4b633f71ffa00b1fc98cd4ec08e5e9705a
Abdullajon <madraximovabdullajon806@gmail.com>
2026-04-13 01:21:43 +0500

    CoderAbuz

--- kinolar/settings.py
+++ kinolar/settings.py
@@ -93,6 +93,8 @@ TIME_ZONE = "Asia/Tashkent"
 USE_I18N = True
 
 USE_TZ = True
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
